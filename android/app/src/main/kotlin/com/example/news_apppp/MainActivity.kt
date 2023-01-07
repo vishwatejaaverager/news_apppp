b135da78573d7754147523c76bee6daef9f83324
vishwatejaaverager <vishuvishwa74@gmail.com>
2023-01-07 22:17:08 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/news_apppp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.news_apppp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/news_home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class NewsHomeScreen extends StatefulWidget {
+  const NewsHomeScreen({super.key});
+
+  @override
+  State<NewsHomeScreen> createState() => _NewsHomeScreenState();
+}
+
+class _NewsHomeScreenState extends State<NewsHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
